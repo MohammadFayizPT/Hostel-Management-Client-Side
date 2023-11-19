7289aa68ba63cd19b18a30e42043b7c74053345d
MohammadFayizPT <mohammadfayizpt@gmail.com>
2023-11-19 08:05:22 +0530

    Created login page0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header/Header';
-import Home from './Pages/Home'
+import Home from './Pages/Home';
 import LeftNavbar from './Components/LeftNavbar/LeftNavbar';
 import Students from './Pages/Students';
 import Staffs from './Pages/Staffs';
@@ -10,20 +10,25 @@ import Complaints from './Pages/Complaints';
 import LoginSignup from './Pages/LoginSignup';
 
 function App() {
+  const location = useLocation();
+
+  // Determine if the current route is the login/signup page
+  const isLoginPage = location.pathname === '/login';
+
   return (
     <div className="App">
-       <Header/>
-       <div>
-       <LeftNavbar/>
-       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/students' element={<Students/>}/>
-        <Route path='/staffs' element={<Staffs/>}/>
-        <Route path='/rooms' element={<Rooms/>}/>
-        <Route path='/complaints' element={<Complaints/>}/>
-        <Route path='/login' element={<LoginSignup/>}/>
-       </Routes>
-       </div>
+      {!isLoginPage && <Header />}
+      <div>
+        {!isLoginPage && <LeftNavbar />}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/students' element={<Students />} />
+          <Route path='/staffs' element={<Staffs />} />
+          <Route path='/rooms' element={<Rooms />} />
+          <Route path='/complaints' element={<Complaints />} />
+          <Route path='/login' element={<LoginSignup />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>
-      <h1>LoginSignup</h1>
+    <div className='loginsignup'>
+      <div className='loginsignup-shadow'></div>
+      <div className="loginsignup-child">
+        <div className="loginsignup-title">
+         <h1>Login Here</h1>
+        </div>
+          <div className="loginsignup-input">
+          <input type="email" placeholder='Your email'/>
+          <input type="password" placeholder='Password' />
+          </div>
+          <button>Submit</button>      
+          <p>Don't have an account?</p>
+          <a href="/">Signup</a>
+      </div>
     </div>
   )
 }
