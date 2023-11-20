3fff3a99302eb90ce1c4e15caa8bb4f9290ef0d9
MohammadFayizPT <mohammadfayizpt@gmail.com>
2023-11-21 01:00:19 +0530

    Complaints page

--- src/Pages/Complaints.jsx
+++ src/Pages/Complaints.jsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import "./CSS/Complaints.css";
 
 const Complaints = () => {
-  return (
-    <div>
-      <h1>Complaints</h1>
-    </div>
-  )
-}
+  const [complaints, setComplaints] = useState([]);
 
-export default Complaints
+  useEffect(() => {
+    // Fetch complaints data from the server
+    fetch('http://localhost:3001/complaints')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data); // Check the data structure in the console
+        setComplaints(data);
+      })
+      .catch(error => console.error('Error fetching complaints:', error));
+  }, []);
+
+  return (
+    <div className='complaint'>
+    
+      <h1>Complaints</h1>
+     
+     <div>
+      <table>
+  <thead>
+    <tr className='row-head'>
+      <th>Comp_id</th>
+      <th>Student_id</th>
+      <th>Date</th>
+      <th>Issue</th>
+      <th className='th5'>Status</th>
+    </tr>
+  </thead>
+  <tbody>
+    {complaints.map(complaint => (
+      <tr key={complaint.Comp_id} className="row">
+        <td className='td1'>{complaint.Comp_id}</td>
+        <td className='td2'>{complaint.Student_id}</td>
+        <td className='td3'>{complaint.Date}</td>
+        <td className='td4'>{complaint.Issue}</td>
+        <td className='td5'>{complaint.Status ? "Solved" : "Pending"}</td>
+      </tr>
+    ))}
+  </tbody>
+</table>
+</div>
+
+    </div>
+  );
+};
+
+export default Complaints;
