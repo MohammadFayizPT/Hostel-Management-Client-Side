b708bd94f40dc385de30b4d673bca7671b3d4073
nibaliqbal <nibaliqbal@gmail.com>
2023-11-22 01:25:11 +0530

    Rooms_frontend

--- src/Pages/Rooms.jsx
+++ src/Pages/Rooms.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './CSS/Rooms.css'
+import { RoomDiv } from '../Components/RoomDiv/RoomDiv'
 
 const Rooms = () => {
   return (
-    <div>
-       <h1>Rooms</h1>
+    <div className='rooms'>
+       <h2>Check Room Details</h2>
+       
+       <div className='roomdiv-titles'>
+          <h3>Room no.</h3>
+          <h3>No. of Students</h3>
+          <h3>Status</h3>
+       </div>
+
+        <RoomDiv/>
     </div>
   )
 }
