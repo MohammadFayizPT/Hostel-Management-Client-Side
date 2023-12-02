c2cb98872cdc68dd9d9d279843e62278f8403c1b
GeorgeSkaria <georgeskaria2003@gmail.com>
2023-12-02 22:16:27 +0530

    Staffpage frontend work over

--- src/Pages/Staffs.jsx
+++ src/Pages/Staffs.jsx
@@ -1,49 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import './CSS/Staffs.css'; // Import the CSS file
+import StaffDiv from '../Components/StaffDiv/StaffDiv'; // Import the StaffDiv component if it's in a separate file
 
-import './CSS/StaffDetails.css'; // Import the CSS file
-
-const StaffDetails = () => {
-  const [staffDetails, setStaffDetails] = useState([]);
-
-  useEffect(() => {
-    // Fetch fee payments data from the server
-    fetch('http://localhost:3001/staffs') // Adjust the endpoint URL according to your server
-      .then(response => response.json())
-      .then(data => {
-        console.log(data); // Check the data structure in the console
-        setStaffDetails(data);
-      })
-      .catch(error => console.error('Error fetching staffs:', error));
-  }, []);
-
+const Staffs = () => {
   return (
-    <div className='staff'>
-      <h1>Staff Details</h1>
-      <div>
-        <table>
-          <thead>
-            <tr className='row-headst'>
-              <th>Staff ID</th>
-              <th>Name</th>
-              <th>Type</th>
-              <th>Salary</th>
-            </tr>
-          </thead>
-          <tbody>
-            {staffDetails.map(staff => (
-              <tr key={staff.Staff_id} className='rowst'>
-                <td className='td1'>{staff.Staff_id}</td>
-                <td className='td2'>{staff.Name}</td>
-                <td className='td3'>{staff.Type}</td>
-                <td className='td4'>{staff.Salary}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+    <div className='staffs'>
+        <h2>Check Staff Details</h2>
+        <div className="staffdiv-titles">
+          <h3>Staff ID</h3>
+          <h3>Name</h3>
+          <h3>Type</h3>
+          <h3>Salary</h3>
+        </div>
+        <StaffDiv />
       </div>
-    </div>
   );
-  
 };
 
-export default StaffDetails;
+export default Staffs;
